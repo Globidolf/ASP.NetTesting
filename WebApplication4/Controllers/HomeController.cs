239b3adfe2c9214ce8f9d43d0f252e434e085b5b
Silvan Pfister <sp@asperger-ag.ch>
2017-12-06 16:14:12 +0100

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			Dictionary<string, string> Data = new Dictionary<string, string> {
+				["One"] = "One",
+				["Two"] = "Two"
+			};
+			ViewData["Test"] = Data;
+			ViewData["Title"] = "TEST";
+			return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+			IActionResult ErrorMessage = View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return ErrorMessage;
+        }
+    }
+}
